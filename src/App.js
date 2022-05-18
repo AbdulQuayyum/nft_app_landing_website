35c7f0f40ae1a07157dcbcdda2832c21b815c6dd
Abdul Quayyum <alaoabdulquayyumm@gmail.com>
2022-05-18 12:14:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -59,6 +59,26 @@ const App = () => {
         mockupImg={assets.homeHero}
         banner="banner"
       />
+      <SectionWrapper
+        title="Smart User Interface MarketPlace"
+        description="Experience a buttery UI of Soft-NFTs Marketplace. Smooth constant colors of a fluent UI design."
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+      <SectionWrapper
+        title="Deployment"
+        description="Soft-NFTs is built using Expo which runs natively on all users' devices. You can easily get your app into people's hands"
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper
+        title="Creative way to show case the store"
+        description="The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
     </>
   )
 }
